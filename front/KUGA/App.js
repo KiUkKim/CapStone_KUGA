f903e26510776627add9617f47e77aec5e1371e6
jsc1014 <wotlr5209@gmail.com>
2022-09-18 15:50:59 +0900

    test choi #4

--- front/KUGA/App.js
+++ front/KUGA/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>IssueTest11</Text>
+      <Text>IssueTest555556</Text>
       <StatusBar style="auto" />
     </View>
   );
